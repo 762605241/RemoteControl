ae01ca730d175c9f87c18f21f1552ea79e06cfda
liuxiaoliang <762605241@qq.com>
2019-03-27 15:47:56 +0800

    测试 传输指令、页面同时运行

---
+++ RemoteControl/src/com/lxl/test/SendUI.java
@@ -0,0 +1,64 @@
+package com.lxl.test;
+
+import java.awt.AWTException;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.ImageIcon;
+
+public class SendUI {
+	public static void main(String[] args) {
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		Robot robot = null;
+		ImageIcon img = null;
+		Socket socket = null;
+		ObjectOutputStream oos = null;
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				ServerSocket receiveOrder = null;
+				ObjectInputStream ois = null;
+				Socket socket = null;
+				try {
+					while (true) {
+						receiveOrder = new ServerSocket(8888);
+						socket = receiveOrder.accept();
+						ois = new ObjectInputStream(socket.getInputStream());
+						Object e = ois.readObject();
+						System.out.println(e.toString());
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		try {
+			// 开启两个线程 1接收鼠标事件 2接受键盘事件
+			while (true) {
+				socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 9999);
+				robot = new Robot();
+				img = new ImageIcon(
+						robot.createScreenCapture(new Rectangle(0, 0, (int) d.getWidth(), (int) d.getHeight())));// 截取屏幕
+				oos = new ObjectOutputStream(socket.getOutputStream());
+				oos.writeObject(img);
+				oos.flush();
+				System.out.println("成功发送数据");
+			}
+		} catch (AWTException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
